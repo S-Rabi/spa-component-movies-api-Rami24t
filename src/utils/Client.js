cf509ca26151615d358206f5fbe01be43dc0ed05
S Rabi <rabi.sanaye@yahoo.com>
2022-11-07 16:39:02 +0100

    c

--- src/utils/Client.js
+++ src/utils/Client.js
@@ -1,14 +1,16 @@
 class Client {
-    constructor () {
-        //Your token goes here
-        this.token = "9534e126";
-    }
+  constructor() {
+    //Your token goes here
+    this.token = "43648523";
+  }
 
-    async getMovieData(movie) {
-        let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`);
-        let data = await response.json();
-        return data;
-    }
+  async getMovieData(movie) {
+    let response = await fetch(
+      `http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`
+    );
+    let data = await response.json();
+    return data;
+  }
 }
 
 export default Client;
